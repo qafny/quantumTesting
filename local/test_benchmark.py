6be187dc3d210dc2921b59f54d3fe3760cb4f877
Chandeepa Dissanayake <chandeepadissanayake@gmail.com>
2026-05-27 10:33:09 -0500

    Added the basis to the test files.

--- local/test_benchmark.py
+++ local/test_benchmark.py
@@ -1,15 +1,16 @@
 import parser.utils.benchmark_utils as benchmark_utils
+from evaluators.basis import QiskitBasis
 from parser.utils import qiskit_utils
 from qetast.printers import QuantumCircuitPrinter
 from qetast.processors import PrecedingHadamardEliminationProcessor, SucceedingHadamardEliminationProcessor
 
-benchmark_path = "benchmarks/qiskit"
+benchmark_path = "benchmarks/cdkm_ripple_carry_adder"
 
 circuits = benchmark_utils.read_benchmark(benchmark_path)
 qc = circuits[0]
 qiskit_utils.visualize_qiskit_circuit(qc, title="Input QC")
 
-ast = qiskit_utils.parse_qiskit_circuit(qc, ["cx", "h", "s", "t"])
+ast = qiskit_utils.parse_qiskit_circuit(qc, QiskitBasis())
 
 procs = [
     PrecedingHadamardEliminationProcessor(),
--- local/test_parser.py
+++ local/test_parser.py
@@ -1,4 +1,5 @@
 import parser.utils.qiskit_utils as qiskit_utils
+from evaluators.basis import QiskitBasis
 from qetast.printers import QuantumCircuitPrinter
 from qetast.processors import PrecedingHadamardEliminationProcessor, SucceedingHadamardEliminationProcessor
 
@@ -7,7 +8,7 @@ benchmark_path = "benchmarks/custom/ghz"
 qc = qiskit_utils.read_qiskit_custom_benchmark(benchmark_path)
 qiskit_utils.visualize_qiskit_circuit(qc, title="Input QC")
 
-ast = qiskit_utils.parse_qiskit_circuit(qc, ["cx", "h", "s", "t"])
+ast = qiskit_utils.parse_qiskit_circuit(qc, QiskitBasis())
 
 procs = [
     PrecedingHadamardEliminationProcessor(),
