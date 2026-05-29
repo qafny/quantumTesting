cb809abd8a736304fed49719abd7753813260b0c
liyili2 <liyili2@illinois.edu>
2026-05-29 14:38:59 -0500

    update.

--- qetast/simulators.py
+++ qetast/simulators.py
@@ -1,59 +1,89 @@
+import cmath
 import math
 from typing import Dict
 from qetast.nodes import QXH, QXRZ, QXX, QXCU
 from qetast.program import QETASTVisitor
 from qetast.values import CoqNVal, CoqYVal, exchange
 
+def merge_aux(x: tuple[float,dict], y:list[tuple[float,dict]]):
+
+    test = False
+    tmp = []
+    for (q,p) in y:
+        if x[1] == p:
+            test = True
+            tmp.append((x[0] + q,p))
+        else:
+            tmp.append((q,p))
+    return test,tmp
+
+def merge(y:list[tuple[float,dict]]):
+
+    re = []
+    while len(y) > 0:
+        value = y.pop(0)
+        test,y = merge_aux(value, y)
+        if not test:
+            re.append(value)
+    return re
+
 
 class QETSimulator(QETASTVisitor):
 
-    def __init__(self, state: Dict, environment: Dict):
+    def __init__(self, state: list[tuple[complex, dict]]):
         self.state = state
-        self.environment = environment
 
     def visitH(self, node: QXH):
         idx = node.qubit()
-        x = self.state.get(idx)
 
-        if isinstance(x, CoqNVal):
-            if x:
-                self.state[idx] = CoqYVal([(math.sqrt(2)/2, 0)],[(-math.sqrt(2)/2, 0)])
+        tmp = []
+        for (x,y) in self.state:
+            if y.get(idx):
+                tmp.append((- math.sqrt(2)/2 * x,y.update({idx:True})))
+                tmp.append((math.sqrt(2)/2 * x,y.update({idx:False})))
             else:
-                self.state[idx] = CoqYVal([(math.sqrt(2)/2, 0)],[(math.sqrt(2)/2, 0)])
-        elif isinstance(x, CoqYVal):
-            self.state[idx].addH()
+                tmp.append((math.sqrt(2)/2 * x,y.update({idx:True})))
+                tmp.append((math.sqrt(2)/2 * x,y.update({idx:False})))
 
-        self.state[idx] = self.state[idx].simpRy(x)
-
+        tmp = merge(tmp)
+        self.state = merge(tmp)
         return True
 
     def visitX(self, node: QXX):
         idx = node.qubit()
-        x = self.state.get(idx)
-
-        exchange(x)
+        self.state = [(x,y.update({idx: not y.get(idx)})) for (x,y) in self.state]
 
         return True
 
     def visitRZ(self, node: QXRZ):
         idx = node.qubit()
-        x = self.state.get(idx)
         phase = node.phase().value()
 
-        if isinstance(x, CoqNVal):
-            x.setPhase(phase)
-        elif isinstance(x, CoqYVal):
-            x.applyMult(phase)
-        else:
-            return False
+        tmp = []
+        for (x,y) in self.state:
+            if y.get(idx):
+                tmp.append((x*cmath.exp(1j * phase), y))
+            else:
+                tmp.append((x,y))
+        self.state = tmp
 
         return True
 
     def visitCU(self, node: QXCU):
         idx = node.qubit()
-        x = self.state.get(idx)
 
-        if x.getBit():
-            return node.program().accept(self)
-        else:
-            return False
+        re = []
+        it = self.state
+        for i in range(it):
+            if it[i][1].get(idx):
+                tmp = self.state
+                self.state = [it[i]]
+                node.program().accept(self)
+                re.append(self.state)
+                self.state = tmp
+            else:
+                re.append(it[i])
+
+        self.state = re
+
+        return True
