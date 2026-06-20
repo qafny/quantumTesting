47fecdf82b58aad0e34d367993aa9718dcecb956
Chandeepa Dissanayake <chandeepadissanayake@gmail.com>
2026-06-20 16:19:25 +0530

    Minor structural change to the local files

--- local/run_qet_evaluator.py
+++ local/run_qet_evaluator.py
@@ -1,15 +1,15 @@
 import parser.utils.qiskit_utils as qiskit_utils
-from evaluators.tsim import TSimEvaluator
+from evaluators.qet import QETEvaluator
 
-benchmark_path = "benchmarks/testing/custom/oneh"
+benchmark_path = "benchmarks/testing/custom/onecu"
 
 input_state = {
-    "0": False,
+    "0": True,
     "1": False,
 }
 
 qc = qiskit_utils.read_qiskit_custom_benchmark(benchmark_path)
-evaluator = TSimEvaluator(qc)
+evaluator = QETEvaluator(qc)
 out_state = evaluator.evaluate(input_state)
 
 print(out_state)
