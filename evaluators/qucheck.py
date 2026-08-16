60831ff854f04308c548426070df0632694d5011
Rishita Sarkar <u7919549@anu.edu.au>
2026-08-16 19:44:25 +1000

    Qucheck evaluator implementation

--- evaluators/qucheck.py
+++ evaluators/qucheck.py
@@ -4,12 +4,7 @@ from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 from evaluators.base import BaseEvaluator
 from evaluators.basis import QETGateSetBasis
-from qetast.simulators import QETSimulator
-from qucheck.coordinator import Coordinator
-from qucheck.property import Property
-from qucheck.test_runner import TestRunner
 import evaluators.utils as eval_utils
-import helpers.qubits as helper_qubits
 
 
 class QuCheckEvaluator(BaseEvaluator):
@@ -31,29 +26,29 @@ class QuCheckEvaluator(BaseEvaluator):
     def evaluate(self, ins: Dict[str, bool]) -> List[Tuple[complex, Dict[str, bool]]]:
         logging.info("Evaluating using QuCheckEvaluator")
 
-        '''
-        Currently, we simply use the QETSimualator and return the results. But,
-        TODO:
-            1. Prapare the initial state using the ins dict.
-            2. Run the AerSimulator on the prepared initial state
-            3. Store the properties for the use in the comparator
+        n_qubits = self.get_parsed_circuit().num_qubits
 
-            Docs:  https://qiskit.github.io/qiskit-aer/stubs/qiskit_aer.AerSimulator.html
-        '''
+        qc = QuantumCircuit(n_qubits)
+        for i, bit in ins.items():
+            if bit:
+                qc.x(int(i))
+        qc.compose(self.get_parsed_circuit(), inplace=True)
 
-        initial_state = helper_qubits.get_system_state_from_qubits(ins)
-
-        simulator = QETSimulator(initial_state)
-        simulator.visitRoot(self.get_circuit_ast())
+        qc.save_statevector()
+        job = self.backend.run(qc)
+        result = job.result()
+        sv = result.get_statevector(qc)
 
         state = []
-        for (amp, sd) in simulator.state:
+        for i, amp in enumerate(sv):
             amp = eval_utils.zcomplex(amp)
 
             # We eliminate 0 amplitude basis-kets.
             if amp == 0:
                 continue
-
+            sd = {}
+            for q in range(n_qubits):
+                sd[str(q)] = bool((i >> q) & 1)
             state.append((amp, sd))
 
         logging.info("Finished Evaluating using QuCheckEvaluator")
