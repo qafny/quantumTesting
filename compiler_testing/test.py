663be2db6459a1c3b778eb53f5ac1982a510ad6d
var-nan <krs.nandhan999@gmail.com>
2026-09-14 17:38:16 -0500

    test file

--- compiler_testing/test.py
+++ compiler_testing/test.py
@@ -2,16 +2,18 @@ from program_generator.generators.random_circuit_generator import generate_rando
 from compiler_testing.config.basis_sets import BASIS_SET_1, BASIS_SET_2
 from compiler_testing.runners.transpiler_test_runner import run_transpiler_tests
 from compiler_testing.reporting.summary_printer import print_summary
+from qiskit import qasm3
 
 
 def main():
     programs = generate_random_programs(
-        num_qubits=8,
-        num_gates=100,
-        generation_gates=["x", "mcx","cx"],
-        n_samples=50,
+        num_qubits=4,
+        num_gates=6,
+        generation_gates=["mcx"],
+        n_samples=1,
         seed=42,
     )
+    print(programs[0].circuit)
 
     results = run_transpiler_tests(
         programs=programs,
@@ -19,8 +21,7 @@ def main():
         print_each=True,
     )
 
-    print_summary(results)
-
+    # print_summary(results)
 
 if __name__ == "__main__":
     main()
